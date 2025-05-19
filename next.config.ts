061f63dd9b08efeaba10fd5912f5dca28b6d5990
ArushYadlapati <arush.yadlapati@gmail.com>
2025-05-19 15:28:34 -0700

    Add assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     basePath: "/lolesports-live",
-    output: "export",  // <=== enables static exports
+    assetPrefix: "/lolesports-live",
+    output: "export",
     reactStrictMode: true,
 };
 
