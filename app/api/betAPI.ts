0cf154f30224d06694b0c6c8f4162dd464453905
ArushYadlapati <arush.yadlapati@gmail.com>
2025-07-01 12:27:27 -0700

    Update betAPI.ts

--- app/api/betAPI.ts
+++ app/api/betAPI.ts
@@ -3,7 +3,8 @@
  */
 // TODO: Fix so that it doesn't get cooked by rematches (double elims)
 export const betNameMap: { [key: string]: string } = {
-    "G2 Esports vs FURIA": "https://www.co.bet365.com/?_h=nraabFD5b_3Wc3-Ws2BRtA%3D%3D&btsffd=1#/AC/B151/C21065353/D19/E22997012/F19/",
-    "BILIBILI GAMING DREAMSMART vs GAM Esports": "https://www.co.bet365.com/?_h=nraabFD5b_3Wc3-Ws2BRtA%3D%3D&btsffd=1#/AC/B151/C21065353/D19/E22997013/F19/",
-    "BILIBILI GAMING DREAMSMART vs G2 Esports": "https://www.co.bet365.com/?_h=nraabFD5b_3Wc3-Ws2BRtA%3D%3D&btsffd=1#/AC/B151/C21065353/D19/E23082926/F19/",
+    "Gen.G Esports vs G2 Esports": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23098727/F19/",
+    "Anyone's Legend vs FlyQuest": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23098728/F19/",
+    "CTBC Flying Oyster vs T1": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23098729/F19/",
+    "Movistar KOI vs BILIBILI GAMING DREAMSMART": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23098730/F19/",
 };
