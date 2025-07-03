07b96d60529214cc864a4f33ebbb20dbf01d5512
ArushYadlapati <arush.yadlapati@gmail.com>
2025-07-03 10:59:57 -0700

    new bet hooks

--- app/api/betAPI.ts
+++ app/api/betAPI.ts
@@ -3,8 +3,8 @@
  */
 // TODO: Fix so that it doesn't get cooked by rematches (double elims)
 export const betNameMap: { [key: string]: string } = {
-    "Gen.G Esports vs G2 Esports": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23098727/F19/",
-    "Anyone's Legend vs FlyQuest": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23098728/F19/",
     "CTBC Flying Oyster vs T1": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23098729/F19/",
     "Movistar KOI vs BILIBILI GAMING DREAMSMART": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23098730/F19/",
+    "G2 Esports vs FlyQuest": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23123384/F19/",
+    "Gen.G Esports vs Anyone's Legend": "https://www.co.bet365.com/#/AC/B151/C21065353/D19/E23123385/F19/"
 };
